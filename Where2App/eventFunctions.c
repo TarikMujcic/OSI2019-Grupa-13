bf4fd172bed63757f5b1acec67f9b755756206d5
Danilo Dedic <danilodedic@hotmail.com>
2020-01-08 23:25:57 +0100

    DD - quiz added

--- Where2App/eventFunctions.c
+++ Where2App/eventFunctions.c
@@ -162,3 +162,93 @@ void deleteEvent(int eventID)
     else
         return printf("No event with a matching ID has been found!\n");
 }
+
+
+void configureQuiz()
+{
+    FILE *quizFile=NULL;
+    int n=10,i;
+    char optionHelp[20];
+    QUESTION *quiz=(QUESTION*)calloc(n,sizeof(QUESTION));
+    for(i=0; i<n; i++)
+    {
+        printf("Enter question no. %d: ",i+1);  getchar(); gets(quiz[i].quizQuestion);
+        printf(" Enter the first answer: ");  gets(quiz[i].answer1);
+        printf(" Enter the second answer: "); gets(quiz[i].answer2);
+        printf(" Enter the third answer: ");  gets(quiz[i].answer3);
+        int wrongOption = 0;        //variable to use for help to print out Error if user types in wrong option
+        do
+        {
+            if(wrongOption)
+            {
+                printf("That is not a valid answer! Try again! \n\n");
+                gets(optionHelp);
+            }
+            printf(" WHICH ANSWER IS CORRECT:");
+            scanf("%d",&quiz[i].correctAnswer);
+            wrongOption = 1;
+        }
+        while(quiz[i].correctAnswer < 1 || quiz[i].correctAnswer > 3);
+    }
+    if((quizFile=fopen("Quiz.dat","wb"))!=NULL)
+    {
+        for(i=0; i<n; i++)
+            fwrite(&quiz[i],sizeof(QUESTION),1,quizFile);
+        fclose(quizFile);
+    }
+    else
+    {
+        printf("\nError! Couldn't open 'Quiz.dat'!\n");
+        return;
+    }
+    free(quiz);
+    printf("\n\nYOUR QUIZ WAS SUCCESSFULLY CREATED!\n");
+}
+
+
+void playQuiz()
+{
+    FILE *quizFile=NULL;
+    int score=0,i=0,n=10,answer[n];
+    char optionHelp[20];
+    QUESTION *quiz=(QUESTION*)calloc(n,sizeof(QUESTION)),tmp;
+    if((quizFile=fopen("Quiz.dat","rb"))!=NULL)
+    {
+        while(fread(&tmp,sizeof(QUESTION),1,quizFile))
+            quiz[i++]=tmp;
+        fclose(quizFile);
+    }
+    else
+    {
+        printf("\nError! Couldn't open 'Quiz.dat'. File moved or missing!\n");
+        return;
+    }
+    printf("===============================================\n");
+    for(i=0; i<n; i++)
+    {
+        printf("Question %d: %s\n",i+1,quiz[i].quizQuestion);
+        printf("    1) %s\n",quiz[i].answer1);
+        printf("    2) %s\n",quiz[i].answer2);
+        printf("    3) %s\n",quiz[i].answer3);
+        int wrongOption = 0;        //variable to use for help to print out Error if user types in wrong option
+        do
+        {
+            if(wrongOption)
+            {
+                printf("That is not a valid answer! Try again! \n\n");
+                gets(optionHelp);
+            }
+            printf(" Choose your answer:");
+            scanf("%d",&answer[i]);
+            wrongOption = 1;
+        }
+        while(answer[i]<1 || answer[i]>3);
+        if(answer[i]==quiz[i].correctAnswer)
+            score++;
+    }
+    printf("===============================================\n");
+    if(score==n)
+        printf("CONGRATULATIONS! ");
+    printf("Your score is: %d/%d",score,n);
+    free(quiz);
+}
--- Where2App/eventFunctions.h
+++ Where2App/eventFunctions.h
@@ -18,11 +18,22 @@ typedef struct index
     int idNum,address;
 }INDEX;
 
+typedef struct
+{
+    int correctAnswer;
+    char quizQuestion[100],
+         answer1[25],
+         answer2[25],
+         answer3[25];
+} QUESTION;
+
 int eventIdSearch(int );          //function that checks whether the event exists or not
 int createNewEvent();             //function for creation of events
 void readNewEvent(EVENT *,int);   //function for getting info about new event
 int updateEvent();                //function for updating event info
 void deleteEvent(int eventID);    //function for deleting event
+void playQuiz();                  //function for playing a quiz game
+void configureQuiz();             //function for quiz configuration
 
 
 
